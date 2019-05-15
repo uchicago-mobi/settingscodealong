78811a93e6fac9d00b2567d288594164c3c6567b
Chelsea Troy <chelsea.dommert@gmail.com>
2019-05-15 17:43:29 -0500

    Initial Commit

---
+++ SettingsCodeAlong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SettingsCodeAlong
+//
+//  Created by Chelsea Troy on 5/15/19.
+//  Copyright © 2019 Chelsea Troy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
