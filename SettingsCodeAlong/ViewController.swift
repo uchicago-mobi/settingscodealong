0e9a03e6dd899ff63e9a9df448aed1a08e954929
Chelsea Troy <chelsea.dommert@gmail.com>
2019-05-15 18:43:08 -0500

    Learning about settings bundles

--- SettingsCodeAlong/ViewController.swift
+++ SettingsCodeAlong/ViewController.swift
@@ -13,8 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        UserDefaults.standard.set("1.0", forKey: "VERSION")
+        print(UserDefaults.standard.string(forKey: "VERSION"))
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(defaultsChanged),
+            name: UserDefaults.didChangeNotification,
+            object: nil
+        )
+        defaultsChanged()
     }
-
-
+    
+    @objc func defaultsChanged() {
+        if UserDefaults.standard.bool(forKey: "BLUE_THEME") {
+            self.view.backgroundColor = UIColor.blue
+        } else {
+            self.view.backgroundColor = UIColor.orange
+        }
+    }
 }
 
